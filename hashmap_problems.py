df0a5cdb2c808a5d5aa16d626efb94c3b79e3859
Yulin Zheng <lyra1225@gmail.com>
2023-04-15 14:20:49 -0700

    hashmap

---
+++ hashmap_problems.py
@@ -0,0 +1,6 @@
+def isAnagram_242(s, t):
+    """
+    Given two strings s and t,
+    return true if t is an anagram of s, and false otherwise.
+    """
+    return
---
+++ test_hashmap.py
@@ -0,0 +1,1 @@
+import hashmap_problems
