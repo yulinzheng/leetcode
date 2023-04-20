c05e786de504930466e50a68c4947ee573325bf7
Yulin Zheng <lyra1225@gmail.com>
2023-04-20 16:41:20 -0700

    hashmap (total:18)

--- hashmap_problems.py
+++ hashmap_problems.py
@@ -28,7 +28,7 @@ def isAnagram_242(s, t):
             hashmap_t[letter] = 1
         else:
             hashmap_t[letter] += 1
-    
+
     return hashmap_s == hashmap_t
 
 def commonChars_1002(words):
@@ -91,8 +91,8 @@ def isHappy_202(n):
             or loops in a cycle which does not include 1.
     Numbers for which this process ends in 1 are happy.
 
-    Time: O()
-    Space: O()
+    Time: O(logn)
+    Space: O(logn)
     """
     seen = set()
     while n != 1:
@@ -111,7 +111,58 @@ def isHappy_202(n):
     return True
 
 def twoSum_1(nums, target):
-    return
+    """
+    Given an array of integers nums and an int target,
+    return indices of the two numbers such that
+    they add up to target.
 
-def fourSumCount(nums1, nums2, nums3, nums4):
-    return
+    Time: O(n)
+    Space: O(n)
+    """
+    hashmap = {}
+    for i, num in enumerate(nums):
+        other = target - num
+        if other in hashmap:
+            return [i, hashmap[other]]
+        else:
+            hashmap[num] = i
+    return []
+
+def fourSumCount_454(nums1, nums2, nums3, nums4):
+    """
+    Given four integer arrays nums1, nums2, nums3, nums4
+    all of length n, return the number of tuples (i, j, k, l)
+    such that:
+        0 <= i, j, k, l < n
+        nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+
+    Time: O(n^2)
+    Space: O(n^2)
+    """
+    # map all unique sums of nums1 & nums2
+    # to their number of occurrences
+    sum_1 = {}
+    for num_1 in nums1:
+        for num_2 in nums2:
+            sum_num = num_1 + num_2
+            if sum_num not in sum_1:
+                sum_1[sum_num] = 1
+            else:
+                sum_1[sum_num] += 1
+    # map all unique sums of nums3 & nums4
+    # to their number of occurrences
+    sum_2 = {}
+    for num_3 in nums3:
+        for num_4 in nums4:
+            sum_num = num_3 + num_4
+            if sum_num not in sum_2:
+                sum_2[sum_num] = 1
+            else:
+                sum_2[sum_num] += 1
+    # calculate # of possible sums
+    count = 0
+    for sum_num in sum_1:
+        other = 0 - sum_num
+        if other in sum_2:
+            count += sum_1[sum_num] * sum_2[other]
+    return count
--- test_hashmap.py
+++ test_hashmap.py
@@ -5,13 +5,29 @@ def test_isAnagram_242():
     assert code.isAnagram_242('rat', 'car') == False
 
 def test_commonChars_1002():
-    assert code.commonChars_1002(["bella","label","roller"]) == ["e","l","l"]
-    assert code.commonChars_1002(["cool","lock","cook"]) == ["c","o"]
+    result = code.commonChars_1002(["bella","label","roller"])
+    assert set(result) == set(["e","l","l"])
+    result = code.commonChars_1002(["cool","lock","cook"])
+    assert set(result)== set(["c","o"])
 
 def test_intersection_349():
-    assert code.intersection_349([1, 2, 2, 1], [2, 2]) == [2]
-    assert code.intersection_349([4,9,5], [9,4,9,8,4]) == [9, 4]
+    result = code.intersection_349([1, 2, 2, 1], [2, 2])
+    assert set(result) == set([2])
+    result = code.intersection_349([4,9,5], [9,4,9,8,4])
+    assert set(result) == set([9, 4])
 
 def test_isHappy_202():
     assert code.isHappy_202(19) == True
     assert code.isHappy_202(2) == False
+
+def test_twoSum_1():
+    result = code.twoSum_1([2,7,11,15], 9)
+    assert set(result) == set([0, 1])
+    result = code.twoSum_1([3,2,4], 6)
+    assert set(result) == set([1, 2])
+
+def test_fourSumCount_454():
+    result = code.fourSumCount_454([1,2], [-2,-1], [-1,2], [0,2])
+    assert result == 2
+    result = code.fourSumCount_454([-1,-1], [-1,1], [-1,1], [1,-1])
+    assert result == 6
