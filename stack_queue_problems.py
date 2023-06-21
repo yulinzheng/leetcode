d5c5ab3ac0c978422ce9ae75406ee6448bbb8619
Yulin Zheng <lyra1225@gmail.com>
2023-06-21 16:07:28 -0700

    stack queue (total:31)

--- stack_queue_problems.py
+++ stack_queue_problems.py
@@ -101,3 +101,77 @@ class MyStack225(object):
         :rtype: bool
         """
         return len(self.que) == 0
+
+def isValid_20(s):
+    """
+    Given a string s containing only ( ) { } [ ]
+    determine if the input string is valid.
+
+        An input string is valid if:
+
+            Open brackets must be closed by the same type of brackets.
+            Open brackets must be closed in the correct order.
+            Every close bracket has a corresponding open bracket of the same type.
+    """
+    stack = []
+    for bracket in s:
+        if bracket in ['(', '{', '[']:
+            stack.append(bracket)
+        else:
+            # no open bracket in stack
+            if not stack:
+                return False
+            # check for matches
+            open_bracket = stack.pop()
+            if bracket == ')' and open_bracket != '(':
+                return False
+            elif bracket == '}' and open_bracket != '{':
+                return False
+            elif bracket == ']' and open_bracket != '[':
+                return False
+            else:
+                continue
+    # stack should be empty
+    if stack == []:
+        return True
+    return False
+
+def removeDuplicates_1047(s):
+    """
+    s consists of lowercase English letters.
+    Keep removing adjacent and equal letters until we no longer can.
+    """
+    stack = []
+    for letter in s:
+        if not stack:
+            stack.append(letter)
+        else:
+            prev = stack.pop()
+            if prev != letter:
+                stack.append(prev)
+                stack.append(letter)
+    return "".join(stack)
+
+def evalRPN_150(tokens):
+    """
+    You are given an array of string tokens that represents
+    an arithmetic expression in a Reverse Polish Notation.
+    Return an integer that represents the value of the expression.
+    """
+    stack = []
+    for token in tokens:
+        if token in ['+', '-', '*', '/']:
+            second = stack.pop()
+            first = stack.pop()
+            if token == '+':
+                stack.append(first + second)
+            elif token == '-':
+                stack.append(first - second)
+            elif token == '*':
+                stack.append(first * second)
+            else:
+                # division always truncates toward 0
+                stack.append(int(first / second))
+        else:
+            stack.append(int(token))
+    return stack.pop()
--- test_stack_queue.py
+++ test_stack_queue.py
@@ -19,3 +19,20 @@ def test_mystack_225():
     assert stack.top() == 2
     assert stack.pop() == 2
     assert stack.empty() == False
+
+def test_isValid_20():
+    assert code.isValid_20("()") == True
+    assert code.isValid_20("(]") == False
+    assert code.isValid_20("()[]{}") == True
+    assert code.isValid_20("([]") == False
+    assert code.isValid_20("]]") == False
+
+def test_removeDuplicates_1047():
+    assert code.removeDuplicates_1047("abbaca") == "ca"
+    assert code.removeDuplicates_1047("azxxzy") == "ay"
+
+def test_evalRPN_150():
+    assert code.evalRPN_150(["2","1","+","3","*"]) == 9
+    assert code.evalRPN_150(["4","13","5","/","+"]) == 6
+    tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+    assert code.evalRPN_150(tokens) == 22
