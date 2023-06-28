cfbe3176931aa3c6d8f5adbaa405e79a4c84655a
Yulin Zheng <lyra1225@gmail.com>
2023-06-28 12:02:46 -0700

    binary tree (total:38)

--- binary_tree_problems.py
+++ binary_tree_problems.py
@@ -49,7 +49,32 @@ def maxDepth_104(root):
     right = maxDepth_104(root.right)
     return max(left + 1, right + 1)
 
-def minDepth(root):
+def minDepth_111(root):
     """
     Given a binary tree, find its minimum depth.
+    """
+    if not root:
+        return 0
+    if not root.left and root.right:
+        return 1 + minDepth_111(root.right)
+    if not root.right and root.left:
+        return 1 + minDepth_111(root.left)
+    return 1 + min(minDepth_111(root.left), minDepth_111(root.right))
+
+def countNodes_222(root):
+    """
+    Given the root of a complete binary tree,
+    return the number of the nodes in the tree.
+    """
+    if not root:
+        return 0
+    if not root.left and root.right:
+        return 1 + countNodes_222(root.right)
+    if not root.right and root.left:
+        return 1 + countNodes_222(root.left)
+    return 1 + countNodes_222(root.left) + countNodes_222(root.right)
+
+def isBalanced(root):
+    """
+    Given a binary tree, determine if it is height-balanced.
     """
