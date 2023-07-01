bf2512ef692eb67fb52d57123ecc21ad8b8017c5
Yulin Zheng <lyra1225@gmail.com>
2023-07-01 16:55:01 -0700

    binary tree (total:41)

--- binary_tree_problems.py
+++ binary_tree_problems.py
@@ -43,6 +43,7 @@ def maxDepth_104(root):
     """
     Given the root of a binary tree, return its maximum depth.
     """
+    # note: what we're really calculating here is height, not depth
     if not root:
         return 0
     left = maxDepth_104(root.left)
@@ -53,6 +54,7 @@ def minDepth_111(root):
     """
     Given a binary tree, find its minimum depth.
     """
+    # note: what we're really calculating here is height, not depth
     if not root:
         return 0
     if not root.left and root.right:
@@ -74,7 +76,47 @@ def countNodes_222(root):
         return 1 + countNodes_222(root.left)
     return 1 + countNodes_222(root.left) + countNodes_222(root.right)
 
-def isBalanced(root):
+def isBalanced_110(root):
     """
-    Given a binary tree, determine if it is height-balanced.
+    Given a binary tree, determine if it is height-balanced,
+    meaning for every node, the following is true:
+        abs( height(node.left) - height(node.right) ) <= 1
     """
+    def get_height(node):
+        if not node:
+            return 0
+        left_height = get_height(node.left)
+        if left_height < 0:
+            return -1
+        right_height = get_height(node.right)
+        if right_height < 0:
+            return -1
+        if abs(left_height - right_height) > 1:
+            return -1
+        return max(left_height + 1, right_height + 1)
+    return get_height(root) >= 0
+
+def binaryTreePaths_257(root):
+    """
+    Given the root of a binary tree, return all
+    root-to-leaf paths in any order.
+    """
+    def traverse(curr, path, result):
+        path.append(curr.val)
+        if not curr.left and not curr.right:
+            str_path = "->".join(map(str, path))
+            result.append(str_path)
+            return
+        if curr.left:
+            traverse(curr.left, path, result)
+            path.pop()
+        if curr.right:
+            traverse(curr.right, path, result)
+            path.pop()
+
+    if not root:
+        return []
+    else:
+        result = []
+        traverse(root, [], result)
+        return result
